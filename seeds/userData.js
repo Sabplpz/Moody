6d2be3ffe8845a35371399f38792161983018309
Mia Reid <miareid@Mias-iMac.local>
2023-05-30 19:14:10 -0400

    seeeeeeeds

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,12 +1,24 @@
+const { User } = require('../models');
+
+const userData =
 [
     {
-        "name": "Mia",
-        "email": "miamreid@gmail.com",
-        "password": "password"
+        name: "Mia",
+        email: "miamreid@gmail.com",
+        password: "password"
     },
     {
-        "name": "Sabrina",
-        "email": "sabrinalpz1994@gmail.com",
-        "password": "password1234"
-    }
-]
+        name: "Sabrina",
+        email: "sabrinalpz1994@gmail.com",
+        password: "password1234"
+    },
+    {
+        name: "Sal",
+        email: "sal@hotmail.com",
+        password: "password"
+    },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
