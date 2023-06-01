94005effcf07f9d923499d4700689fe174dbfd14
Sabplpz <sabrinalpz1994@gmail.com>
2023-06-01 18:42:31 -0400

    route changes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
 router.post('/login', async (req, res) => {
   try {
     // Find the user who matches the posted e-mail address
     const userData = await User.findOne({ where: { email: req.body.email } });
+
+    console.log(userData);
 
     if (!userData) {
       res
@@ -14,7 +17,10 @@ router.post('/login', async (req, res) => {
     }
 
     // Verify the posted password with the password store in the database
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = userData.checkPassword(req.body.password);
+
+    console.log(req.body.password);
+    console.log(validPassword);
 
     if (!validPassword) {
       res
@@ -34,6 +40,22 @@ router.post('/login', async (req, res) => {
   } catch (err) {
     res.status(400).json(err);
   }
+});
+
+router.post('/', (req, res) => {
+  User.create(req.body)
+    .then(userData => {
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+
+        res.json(userData);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/logout', (req, res) => {
