2d311405b0e25bca7a646c7aa6db8646f8fb4790
Sabplpz <sabrinalpz1994@gmail.com>
2023-06-03 12:55:18 -0400

    adding new journal entry route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,15 +4,41 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
+
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] }
+    });
+
+    const user = userData.get({ plain: true });
+
     // Pass serialized data and session flag into template
     if(req.session.logged_in) {
       res.render('homepage', { 
+        ...user,
         logged_in: req.session.logged_in 
       });
     } else {
       res.redirect('login')
     }
     
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/newjournal', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] }
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('journal', {
+      ...user,
+      logged_in: true
+    });
   } catch (err) {
     res.status(500).json(err);
   }
