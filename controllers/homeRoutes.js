974ac2e1f44bbd67e461e220cf8c2b04235d473a
Sabplpz <sabrinalpz1994@gmail.com>
2023-06-04 14:27:18 -0400

    creating route for prev handlerbar

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,15 +12,15 @@ router.get('/', withAuth, async (req, res) => {
     const user = userData.get({ plain: true });
 
     // Pass serialized data and session flag into template
-    if(req.session.logged_in) {
-      res.render('homepage', { 
+    if (req.session.logged_in) {
+      res.render('homepage', {
         ...user,
-        logged_in: req.session.logged_in 
+        logged_in: req.session.logged_in
       });
     } else {
       res.redirect('login')
     }
-    
+
   } catch (err) {
     res.status(500).json(err);
   }
@@ -44,15 +44,41 @@ router.get('/newjournal', withAuth, async (req, res) => {
   }
 });
 
+router.get('/prevjournal/:id', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const journalData = await Journal.findOne({
+      where: {
+        user_id: req.session.user_id,
+        id: req.params.id,
+      }
+    });
+
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] }
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('prevJournal', {
+      ...journalData,
+      ...user,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/');
     return;
   }
-   else {
-     res.render('login');
-   }
+  else {
+    res.render('login');
+  }
 });
 
 router.get('/signup', (req, res) => {
@@ -60,9 +86,9 @@ router.get('/signup', (req, res) => {
     res.redirect('/');
     return;
   }
-   else {
-     res.render('login');
-   }
+  else {
+    res.render('login');
+  }
 })
 
 module.exports = router;
